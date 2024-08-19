712bb9389473284b653e90d54891d9430cdc7f1d
Aditiya Ramadhan <38927949+gwinter23@users.noreply.github.com>
2024-08-19 11:45:27 +0700

    Create eprocky94.sh

---
+++ eprocky94.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+echo ##### Update Pakage And Repository Rocky 9.4 ####
+yum update -y
+yum upgrade -y
+
+echo ##### install depedencies standart EP #####
+yum install nano java-17-openjdk tomcat openssl xorg-x11-server-Xvfb cifs-utils krb5-workstation openldap-clients cyrus-sasl-gssapi cyrus-sasl-ldap net-snmp-utils net-snmp policycoreutils-devel lshw -y 
+
+echo #### Install mysql database ####
+yum install mysql-server.x86_64 -y
+
+echo #### Configuring mysqld ####
+systemctl start mysqld
+systemctl enable mysqld
+
+echo # Mengambil password sementara MySQL
+TEMP_PASSWORD=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
+# Mengotomatiskan mysql_secure_installation
+sudo mysql -u root -p"$TEMP_PASSWORD" --connect-expired-password <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Prosperitats12345';
+UNINSTALL PLUGIN validate_password;
+SET GLOBAL validate_password.length = 6;
+SET GLOBAL validate_password.policy = LOW;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+EOF
+
+echo "MySQL secure installation telah selesai secara otomatis."
+
+sudo tee -a /etc/my.cnf.d/mysql-server.cnf > /dev/null <<EOL
+
+####################################
+# ESET Requirement
+max_allowed_packet = 500M
+log_bin_trust_function_creators=1
+innodb_log_file_size = 200M
+innodb_log_files_in_group = 4
+innodb_lock_wait_timeout=600
+####################################
+
+EOL
+
+# Memulai ulang layanan MySQL agar perubahan diterapkan
+sudo systemctl restart mysqld
+
+echo "Konfigurasi telah ditambahkan dan layanan MySQL telah dimulai ulang."
+
+echo ### Install ODBC Driver ####
+yum install unixODBC -y
+yum install mariadb-connector-odbc -y
+
+
+echo ### Download Installer EP dan Part lainnya ###
+echo "###################checkfile era war req data#####################"
+    erawar=era.war
+    if [ -f "$erawar" ];
+    then
+        echo "$erawar has found."
+    else
+        wget https://download.eset.com/com/eset/apps/business/era/webconsole/latest/era.war
+    fi
+
+    echo "###################checkfile era req data####################"
+    server=server-linux-x86_64.sh
+    if [ -f "$server" ];
+    then
+        echo "$server has found."
+    else
+        wget https://download.eset.com/com/eset/apps/business/era/server/linux/latest/server-linux-x86_64.sh
+    fi
+
+    echo "###################checkfile agent req data #####################"
+    agent=agent-linux-x86_64.sh
+    if [ -f "$agent" ];
+    then
+        echo "$agent has found."
+    else
+        wget https://download.eset.com/com/eset/apps/business/era/agent/latest/agent-linux-x86_64.sh
+    fi
+
+    echo "###################checkfile rd data#####################"
+    rd=rdsensor-linux-x86_64.sh
+    if [ -f "$rd" ];
+    then
+        echo "$rd has found."
+    else
+        wget https://download.eset.com/com/eset/apps/business/era/rdsensor/latest/rdsensor-linux-x86_64.sh
+    fi
+    echo
+    echo
+    
+    echo "###################checkfile ESET Bridge req data#####################"
+    eb=eset-bridge.x86_64.bin
+    if [ -f "$eb" ];
+    then
+        echo "$eb has found."
+    else
+        wget wget https://download.eset.com/com/eset/apps/business/ech/latest/eset-bridge.x86_64.bin
+    fi
+    echo
+    echo
+
+echo ### Memberikan Previleges installer ###
+chmod +x server-linux-x86_64.sh
+chmod +x agent-linux-x86_64.sh
+chmod +x rdsensor-linux-x86_64.sh
+chmod +x eset-bridge.x86_64.bin
+
+echo ### Deploy ESET PROTECT dan Part pendukungnya ####
+
+./server-linux-x86_64.sh --skip-license --db-driver="MariaDB" --db-hostname=127.0.0.1 --db-port=3306 --db-admin-username=root --db-admin-password=Prosperitats12345 --server-root-password=Prosperitats12345 --db-user-username=root --db-user-password=Prosperitats12345 --cert-hostname="*" --enable-imp-program
+
+./agent-linux-x86_64.sh --skip-license --hostname=localhost --port=2222 --webconsole-hostname=localhost --webconsole-port=2223 --webconsole-user=administrator --webconsole-password="Prosperitats12345" --cert-auto-confirm --enable-imp-program
+
+./eset-bridge.x86_64.bin --skip-license
+
+./rdsensor-linux-x86_64.sh --skip-license
+
+echo ##### Insert Era War to Tomcat folder #####
+cp era.war /var/lib/tomcat/webapps/
+systemctl restart tomcat 
+ls -lsb /var/lib/tomcat/webapps/
+
+
+
+
+

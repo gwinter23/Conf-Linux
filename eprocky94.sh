ead11a36a891348a1e80a322ec21479ccd50b5c5
Aditiya Ramadhan <38927949+gwinter23@users.noreply.github.com>
2024-08-27 13:53:09 +0700

    Update eprocky94.sh

--- eprocky94.sh
+++ eprocky94.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-echo ##### Update Pakage And Repository Rocky 9.4 ####
+echo "##### Update Pakage And Repository Rocky 9.4 ####"
 yum update -y
 yum upgrade -y
 
-echo ##### install depedencies standart EP #####
-yum install nano java-17-openjdk tomcat openssl xorg-x11-server-Xvfb cifs-utils krb5-workstation openldap-clients cyrus-sasl-gssapi cyrus-sasl-ldap net-snmp-utils net-snmp policycoreutils-devel lshw -y 
+echo "##### install depedencies standart EP #####"
+yum install nano java-17-openjdk tomcat openssl xorg-x11-server-Xvfb cifs-utils krb5-workstation openldap-clients cyrus-sasl-gssapi cyrus-sasl-ldap net-snmp-utils net-snmp policycoreutils-devel lshw wget tar -y 
 
-echo #### Install mysql database ####
+echo "#### Install mysql database ####"
 yum install mysql-server.x86_64 -y
 
-echo #### Configuring mysqld ####
+echo "#### Configuring mysqld ####"
 systemctl start mysqld
 systemctl enable mysqld
 
-echo # Mengambil password sementara MySQL
-TEMP_PASSWORD=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+#echo # Mengambil password sementara MySQL
+#TEMP_PASSWORD=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 # Mengotomatiskan mysql_secure_installation
-sudo mysql -u root -p"$TEMP_PASSWORD" --connect-expired-password <<EOF
+#sudo mysql -u root -p"$TEMP_PASSWORD" --connect-expired-password <<EOF
+sudo mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'ts12345';
 UNINSTALL PLUGIN validate_password;
 SET GLOBAL validate_password.length = 6;
@@ -49,7 +50,7 @@ sudo systemctl restart mysqld
 
 echo "Konfigurasi telah ditambahkan dan layanan MySQL telah dimulai ulang."
 
-echo ### Install ODBC Driver ####
+echo "### Install ODBC Driver ####"
 yum install unixODBC -y
 yum install mariadb-connector-odbc -y
 
@@ -104,28 +105,42 @@ echo "###################checkfile era war req data#####################"
     echo
     echo
 
-echo ### Memberikan Previleges installer ###
+echo "### Memberikan Previleges installer ###"
 chmod +x server-linux-x86_64.sh
 chmod +x agent-linux-x86_64.sh
 chmod +x rdsensor-linux-x86_64.sh
 chmod +x eset-bridge.x86_64.bin
 
-echo ### Deploy ESET PROTECT dan Part pendukungnya ####
+echo "### Deploy ESET PROTECT dan Part pendukungnya ####"
+echo
+echo
 
-./server-linux-x86_64.sh --skip-license --db-driver="MariaDB" --db-hostname=127.0.0.1 --db-port=3306 --db-admin-username=root --db-admin-password=ts12345 --server-root-password=ts12345 --db-user-username=root --db-user-password=ts12345 --cert-hostname="*" --enable-imp-program
+sudo ./server-linux-x86_64.sh --skip-license --db-driver="MariaDB" --db-hostname=127.0.0.1 --db-port=3306 --db-admin-username=root --db-admin-password=ts12345 --server-root-password=ts12345 --db-user-username=root --db-user-password=ts12345 --cert-hostname="*" --enable-imp-program
 
-./agent-linux-x86_64.sh --skip-license --hostname=localhost --port=2222 --webconsole-hostname=localhost --webconsole-port=2223 --webconsole-user=administrator --webconsole-password="ts12345" --cert-auto-confirm --enable-imp-program
+echo "Deploy ESET BRIDGE"
+sudo ./eset-bridge.x86_64.bin --skip-license
 
-./eset-bridge.x86_64.bin --skip-license
+echo "Deploy RD Sensor"
+sudo ./rdsensor-linux-x86_64.sh --skip-license
 
-./rdsensor-linux-x86_64.sh --skip-license
-
-echo ##### Insert Era War to Tomcat folder #####
+echo "##### Insert Era War to Tomcat folder #####"
 cp era.war /var/lib/tomcat/webapps/
+systemctl enable tomcat
 systemctl restart tomcat 
 ls -lsb /var/lib/tomcat/webapps/
 
+echo "#### disable firewald please checking again ####"
+systemctl disable firewalld
+systemctl stop firewalld
+echo 
+echo "jeda 3 menit untuk instalasi agent"
+sleep 3m
+echo "agent deploy"
+echo 
+echo 
+sudo ./agent-linux-x86_64.sh --skip-license --hostname=localhost --port=2222 --webconsole-hostname=localhost --webconsole-port=2223 --webconsole-user=administrator --webconsole-password="ts12345" --cert-auto-confirm --enable-imp-program
 
+echo "ESET PROTECT Installer Script is Finish check history script untuk mencari error atau issue Thanks, Build By RAM"
 
 
 
